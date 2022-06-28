c9848dfc9b02b5b89f2ccee3efa1f6b7dc543806
Nefalit <nefalit7@gmail.com>
2022-06-28 21:02:54 +0300

    mb done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,71 @@
-export const App = () => {
-  return (
-    <h1>Phonebook</h1>
-    <ContactForm ... />
-  
-    <h2>Contacts</h2>
-    <Filter ... />
-    <ContactList ... />
-  );
-};
+import { Component } from 'react';
+import { nanoid } from 'nanoid';
+import ContactForm from './ContactForm/ContactForm';
+import Filter from './Filter/Filter';
+import ContactList from './ContactList/ContactList';
+
+export class App extends Component {
+  state = {
+    contacts: [
+      { id: nanoid(), name: 'Rosie Simpson', number: '459-12-56' },
+      { id: nanoid(), name: 'Hermione Kline', number: '443-89-12' },
+      { id: nanoid(), name: 'Eden Clements', number: '645-17-79' },
+      { id: nanoid(), name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+  addContact = ({ name, number }) => {
+    const { contacts } = this.state;
+    if (contacts.some(el => el.name === name)) {
+      return alert(`${name} is already in contacts.`);
+    }
+    const contact = {
+      id: nanoid(),
+      name,
+      number,
+    };
+
+    this.setState(({ contacts }) => ({
+      contacts: [contact, ...contacts],
+    }));
+  };
+
+  removeContact = id => {
+    this.setState(({ contacts }) => ({
+      contacts: contacts.filter(contact => contact.id !== id),
+    }));
+  };
+
+  handleFilter = ({ target }) => {
+    this.setState({
+      filter: target.value,
+    });
+  };
+
+  getFilteredContact = () => {
+    const { contacts, filter } = this.state;
+    if (!filter) {
+      return contacts;
+    }
+    const normalizeInput = filter.toLowerCase();
+    const renderArr = contacts.filter(el =>
+      el.name.toLowerCase().includes(normalizeInput)
+    );
+    return renderArr;
+  };
+  render() {
+    const { addContact, handleFilter, removeContact } = this;
+    const { filter } = this.state;
+    const contacts = this.getFilteredContact();
+    return (
+      <div>
+        <h1 className='titleOne'>Phonebook</h1>
+        <ContactForm onSubmit={addContact} />
+        <h2 className='titleTwo'>Contacts</h2>
+        <Filter value={filter} onChange={handleFilter} />
+        <ContactList contacts={contacts} removeContact={removeContact} />
+      </div>
+    );
+  }
+}
